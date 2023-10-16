dc72ab0617703f751baee1662903f83abb89b52d
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2023-10-16 22:05:44 +0900

    Feat:: board & reply Entity

---
+++ community/src/main/java/project/community/entity/Board.java
@@ -0,0 +1,51 @@
+package project.community.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.GenerationType.*;
+
+/**
+ * @author jeonghwanlee
+ * @date 2023-10-15
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Board extends BaseEntity{
+
+    @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "borad_id")
+    private Long id;// 번호
+
+    @Column(nullable = false, length = 100)
+    private String title;// 제목
+
+    @Lob
+    private String contents;// 내용
+
+    @ColumnDefault(value = "0")
+    private Long viewCount;// 조회 수
+
+    @ColumnDefault(value = "0")
+    private String thumbsUpCount;// 좋아요
+
+    @ColumnDefault(value = "0")
+    private String thumbsDownCount;// 싫어요
+
+    @ColumnDefault(value = "false")
+    private boolean isDelete;// 삭제 여부
+
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+
+    @OneToMany(mappedBy = "board")
+    private List<Reply> replies = new ArrayList<>();
+}
