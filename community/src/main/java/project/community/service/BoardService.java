954bb8b198c302c3a790d22113370c5b07c419e7
LeeJeongHwan <70786235+lejehwan@users.noreply.github.com>
2023-11-13 21:54:24 +0900

    Fix:: BoardService 메소드 분리 및 게시글 저장에 대한 예외 추가(dependency::commons-lang3추가)

--- community/src/main/java/project/community/service/BoardService.java
+++ community/src/main/java/project/community/service/BoardService.java
@@ -1,6 +1,7 @@
 package project.community.service;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -22,8 +23,19 @@ public class BoardService {
     private final BoardRepository boardRepository;
 
     @Transactional(readOnly = true)
-    public BoardRespDto findById(Long id) {
-        return new BoardRespDto(boardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("The board can't be found")));
+    public Board findById(Long id) {
+        return boardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("The board can't be found"));
+    }
+
+    /**
+     * 게시글 아이디로 조회한 데이터를 응답 데이터로 감싸 리턴
+     * @date 2023-11-13
+     * @author jeonghwanlee
+     * @param id 게시글 아이디
+     * @return 게시글 응답 데이터
+     */
+    public BoardRespDto findByIdToResp(Long id) {
+        return new BoardRespDto(findById(id));
     }
 
     @Transactional(readOnly = true)
@@ -32,11 +44,14 @@ public class BoardService {
     }
 
     public Board save(Board board) {
+        if (StringUtils.isBlank(board.getTitle())) {
+            throw new IllegalArgumentException("Title is required");
+        }
         return boardRepository.save(board);
     }
 
     public Board updateById(Long id, BoardReqDto boardReqDto) {
-        Board board = boardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("The board can't be found"));
+        Board board = findById(id);
         board.update(boardReqDto);
         return boardRepository.save(board);
     }
